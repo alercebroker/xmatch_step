7c7db28a1141571bf25d30fb445d0828c7c2c099
Pedro Gallardo <pedrogallardorobinson@gmail.com>
2023-08-31 12:31:59 -0400

    chore: added logging to request to info level

--- xmatch_step/step.py
+++ xmatch_step/step.py
@@ -188,7 +188,7 @@ class XmatchStep(GenericStep):
         :param retries_count: number of attempts
         :return: Data with its xmatch. Data without xmatch is not included.
         """
-        self.logger.debug(input_catalog)
+        self.logger.info(input_catalog)
 
         if retries_count > 0:
             try:
